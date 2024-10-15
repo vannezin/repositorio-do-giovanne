593a42203ec979fd2082762c207263e5e737b8b3
Giovanne <136720614+vannezin@users.noreply.github.com>
2024-10-15 09:12:22 -0300

    Add files via upload

--- pergunats.js
+++ pergunats.js
@@ -15,3 +15,9 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Lingua inglesa'
+    'Como se diz oi em ingles?',
+    'Oi em ingles é HI (RAI)'
+)
